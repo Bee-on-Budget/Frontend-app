43a6acbf2f9dcee9a9e7c9619f6bff3229c783e8
Laith1546 <mjhshf12@gmail.com>
2024-12-13 20:37:00 +0200

    Added temporary home page

---
+++ src/Pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        home page
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [ 
+    { path: '/', component: () => import("./Pages/HomePage.vue") },
     { path: '/login', component: () => import("./Pages/LoginPage.vue") }
 ];
 
